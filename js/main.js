445c18216a3216c59c90b0f02934d9aa3d054667
Gaurav Bansal <bansalgaurav726@gmail.com>
2021-03-06 14:53:44 +0530

    added contact form

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
-// Configure carousel Slider
-$(".carousel").carousel({
-  interval: 4000
-});
-
-// Slick Slider
-$(".slider").slick({
-  infinite: true,
-  slideToShow: 1,
-  slideToScroll: 1,
-});
-
-// z-index of card
-document.querySelector('#services').addEventListener('mousemove', function(e) {
-  if(e.target.classList.contains('card-body')) {
-    e.target.lastChild.previousElementSibling.style.zIndex = 1
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('.navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('.navbar').style.opacity = 1;
   }
 });
